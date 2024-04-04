1d2314e972a94dcc607f4dbad1166e877ff5ea80
Naim Apaza <naimlautaroapaza@gmail.com>
2024-04-03 22:44:06 -0300

    Ejercicio 1 terminado

---
+++ TP1_LautaroApaza/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1 = 8;
+		int num2 = 5;
+		int num3 = 1;
+		int num4 = 20;
+		int num5 = 9;
+		
+		float promedio = (float)(num1 + num2 + num3 + num4 + num5) / 5;
+		
+		System.out.println("El promedio es: " + promedio);
+	}
+
+}
